59f16ab0e490c3e7ff9061575551dc23340f9bb7
Gabriel Garcia <ghgarciar@gmail.com>
2022-03-24 10:14:52 -0300

    To be deployed

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-import 'dotenv/config'
-import path from 'path'
+import 'dotenv/config';
+import https from 'https';
+import axios from 'axios';
+import path from 'path';
 import  express  from "express";
+import { Http2ServerRequest } from 'http2';
+
+var client_id = process.env.CLIENT_ID;
+var client_secret = process.env.CLIENT_SECRET;
 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/token',async(req, res) => {
+
+    const resp = await axios.post('https://accounts.spotify.com/api/token',
+    new URLSearchParams({'grant_type':'client_credentials'}),
+
+    {
+      headers:{
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')
+      }
+    }
+    
+    )
+        
+   res.send({
+     ok:true,
+     token: resp.data.access_token
+   });
+      
+    
+
+});
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Escuchando conexiones en el puerto ${process.env.PORT}`)
